50fbd2a4ed516952272c9c92972f8bb32c5e33fd
JiWonJang0514 <82877779+JiWonJang0514@users.noreply.github.com>
2022-06-06 21:44:02 +0900

    commit

--- work/MbtiProject/WebContent/query.sql
+++ work/MbtiProject/WebContent/query.sql
@@ -1,3 +1,10 @@
+drop table users;
+drop table mbti_match;
+
+select * from users;
+select * from mbti_match;
+
+-- 젠더는 f 여자, m 남자
 create table users(
 	id varchar(50),
 	pw varchar(50),
@@ -7,15 +14,58 @@ create table users(
 	mbti varchar(50)
 );
 
-create table mbti(
+--궁합 검색시, mbti_1이 사용자 엠비티아이 같은것으로 셀렉트할 것
+create table mbti_match(
 	mbti_1 varchar(50),
-	mbti_2 varchar(50),
-	match varchar(50),
-	explain varchar(1000)
+	mbti_2 varchar(50)
 );
 
-insert into users values('wendy05', '1234', '장장장', 18, 'f', 'INFJ');
+insert into users values('candy05', '1234', '홍길동', 18, 'm', 'INFJ');
 
-select * from users;
-select * from mbti;
-select * from explains;
+insert into mbti_match values('INFP', 'ENFJ');
+insert into mbti_match values('INFP', 'ENTJ');
+
+insert into mbti_match values('ENFP', 'INFJ');
+insert into mbti_match values('ENFP', 'INTJ');
+
+insert into mbti_match values('INTJ', 'ENFP');
+insert into mbti_match values('INTJ', 'ENTP');
+
+insert into mbti_match values('ENFJ', 'INFP');
+insert into mbti_match values('ENFJ', 'ISFP');
+
+insert into mbti_match values('INTJ', 'ENFP');
+insert into mbti_match values('INTJ', 'ENTP');
+
+insert into mbti_match values('ENTJ', 'INFP');
+insert into mbti_match values('ENTJ', 'INTP');
+
+insert into mbti_match values('INTP', 'ENTJ');
+insert into mbti_match values('INTP', 'ESTJ');
+
+insert into mbti_match values('ENTP', 'INFJ');
+insert into mbti_match values('ENTP', 'INTJ');
+
+insert into mbti_match values('ISFP', 'ENFJ');
+insert into mbti_match values('ISFP', 'ESFJ');
+
+insert into mbti_match values('ESFP', 'ISFJ');
+insert into mbti_match values('ESFP', 'ISTJ');
+
+insert into mbti_match values('ISTP', 'ESFJ');
+insert into mbti_match values('ISTP', 'ESTJ');
+
+insert into mbti_match values('ESTP', 'ISFJ');
+insert into mbti_match values('ESTP', 'ISTJ');
+
+insert into mbti_match values('ISFJ', 'ESFP');
+insert into mbti_match values('ISFJ', 'ESTP');
+
+insert into mbti_match values('ESFJ', 'ISFP');
+insert into mbti_match values('ESFJ', 'ISTP');
+
+insert into mbti_match values('ISTJ', 'ESFP');
+insert into mbti_match values('ISTJ', 'ESTP');
+
+insert into mbti_match values('ESTJ', 'ISFP');
+insert into mbti_match values('ESTJ', 'ISTP');
